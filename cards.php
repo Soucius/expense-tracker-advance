cc3f0f7af17c3289798f8cecf357561abb73179a
Soucius <soucius.ggwp@gmail.com>
2024-09-01 19:59:13 +0300

    card modal

--- cards.php
+++ cards.php
@@ -9,7 +9,7 @@
             <div class="card">
                 <div class="card-header text-center">
                     <h1 class="card-title display-4"><b>Kartlar</b></h1>
-                    <a href="/addCard.php" class="btn btn-primary">Kart Ekle</a>
+                    <a href="functions/addCard.php" class="btn btn-primary">Kart Ekle</a>
                 </div>
 
                 <div class="card-body">
@@ -20,12 +20,29 @@
                             <h5>**** **** **** 3458</h5>
 
                             <div class="row justify-content-center">
-                                <div class="col-6">
-                                    <a href="/editCard" class="btn btn-warning btn-sm">Güncelle</a>
-                                </div>
+                                <div class="col">
+                                    <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">İşlemler</button>
 
-                                <div class="col-6">
-                                    <a href="/deleteCard" class="btn btn-danger btn-sm">Sil</a>
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="exampleModalLabel">İşlemler</h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Kapat"></button>
+                                                </div>
+
+                                                <div class="modal-body">
+                                                    ...
+                                                </div>
+
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-success">Gelir</button>
+                                                    <button type="button" class="btn btn-danger">Gider</button>
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Kapat</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
